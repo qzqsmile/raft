857fa2950af8b7e808ba02ed07440cb7d1f18976
q <qzqsmile@gmail.com>
2020-07-06 22:42:37 +0800

    draft version for 3A

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,6 +1,9 @@
 package raftkv
 
-import "labrpc"
+import (
+	"labrpc"
+	"time"
+)
 import "crypto/rand"
 import "math/big"
 
@@ -8,6 +11,10 @@ import "math/big"
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+
+	lastLeader  int
+	cid         int64
+	seq         int
 }
 
 func nrand() int64 {
@@ -21,6 +28,8 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.seq = 0
+	ck.cid = nrand()
 	return ck
 }
 
@@ -39,6 +48,35 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
+	getArgs := GetArgs{Key: key, Cid:ck.cid, Seq:ck.seq}
+	reply := GetReply{}
+
+	for {
+		doneCh := make(chan bool, 1)
+		go func() {
+			//发送Get() RPC
+			ok := ck.servers[ck.lastLeader].Call("KVServer.Get", &getArgs, &reply)
+			doneCh <- ok
+		}()
+
+		select {
+		case <-time.After(600 * time.Millisecond):
+			DPrintf("clerk(%d) retry PutAppend after timeout\n", ck.cid)
+			continue
+		case ok := <- doneCh:
+			//收到响应后，并且是leader返回的，那么说明这个命令已经执行了
+			if ok && reply.WrongLeader == false {
+				//请求序列号加1
+				ck.seq++
+				return reply.Value
+			}
+		}
+
+		//换一个server重试
+		ck.lastLeader++
+		ck.lastLeader %= len(ck.servers)
+	}
+
 	return ""
 }
 
@@ -54,6 +92,32 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	args := PutAppendArgs{Key: key, Value: value, Op:op, Cid: ck.cid, Seq: ck.seq}
+	reply := PutAppendReply{}
+	for {
+		doneCh := make(chan bool, 1)
+		go func() {
+			//发送Get() RPC
+			ok := ck.servers[ck.lastLeader].Call("KVServer.PutAppend", &args, &reply)
+			doneCh <- ok
+		}()
+
+		select {
+		case <-time.After(600 * time.Millisecond):
+			DPrintf("clerk(%d) retry PutAppend after timeout\n", ck.cid)
+			continue
+		case ok := <- doneCh:
+			//收到响应后，并且是leader返回的，那么说明这个命令已经执行了
+			if ok && reply.WrongLeader == false {
+				//请求序列号加1
+				ck.seq++
+				return
+			}
+		}
+
+		ck.lastLeader++
+		ck.lastLeader %= len(ck.servers)
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
